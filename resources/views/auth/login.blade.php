f00d25b59ccf8d756d77614ff55751ad021e6c73
ntyrtype <123.falah.fajrulahmad@gmail.com>
2026-04-08 15:01:11 +0700

    updateloginregister

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+    @section('title', 'Login') <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <form method="POST" action="{{ route('login') }}">
         @csrf
@@ -31,8 +31,10 @@
                 <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
             </label>
         </div>
+        
 
         <div class="flex items-center justify-end mt-4">
+            
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
@@ -43,5 +45,8 @@
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
+        <a href="{{ route('register') }}" class="text-sm text-blue-600 hover:text-blue-800 hover:underline font-medium">
+            Don't have an account? Register here
+        </a>
     </form>
 </x-guest-layout>
